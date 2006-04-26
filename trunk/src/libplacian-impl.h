eec212706cb72d0ade7c8f129d125365ccfac90c
vishna <vishna@1c5fbee2-3212-0410-80d3-b5e9257d0ed2>
2006-04-26 13:38:57 +0000

    libplacian.h - now only this file is to be included instead of all others

--- trunk/src/libplacian-impl.h
+++ trunk/src/libplacian-impl.h
@@ -1,13 +1,7 @@
-#include "../libplacian/discrete_model.cpp"
-#include "../libplacian/fraction.cpp"
-#include "../libplacian/iprobe.cpp"
-#include "../libplacian/model.cpp"
-#include "../libplacian/poly.cpp"
+#include "../libplacian/libplacian.h"
 
 template class discrete_model<double>;
 template class fraction<double>;
 template class iprobe<double>;
 template class model<double>;
 template class polynomian<double>;
-
-#include "../libplacian/discrete_borel.cpp"
